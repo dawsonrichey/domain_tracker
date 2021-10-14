553eadda4f9425ad41fe645c6609743920b5817d
Dawson Richey <dawsonrichey@gmail.com>
2021-10-14 03:43:36 -0400

    confused trying to build login

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # domain_tracker
+
+FILE STRUCTURE
+
+\-- phplogin
+  |-- index.html
+  |-- style.css
+  |-- authenticate.php
+  |-- logout.php
+  |-- home.php
+  |-- profile.php
---
+++ users/index.php
@@ -0,0 +1,40 @@
+<?php header("Location: login.php"); ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List of Domains</title>
+
+</head>
+<body>
+
+    <h1>List of Domains</h1>
+    <h2> Home Page </h2>
+    code-daddy.com
+    dawsonrichey.com
+    dawsons-geek.com
+    codehoney.dev
+    codehoney.blog
+    bendoverandillshowyoucode.com
+
+
+    http://dawsonrichey.com/
+
+
+
+    https://secure.webhostingpad.com/billing/clientarea.php?action=domains
+	dawsonrichey.com	4th July 2015	4th July 2022	 Enabled	
+Transferred Away
+
+    dawson.alexander.richey@gmail.com
+    Miws1989mi
+
+
+    https://business16.web-hosting.com:2083/cpsess3939586981/frontend/paper_lantern/index.html?login=1&post_login=86032887738078
+</body>
+</html>
